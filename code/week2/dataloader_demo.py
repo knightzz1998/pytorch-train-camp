71f69c95edbf6e8574a92df2971131f29c3811b2
王天赐 <15565946702@163.com>
2021-10-25 16:24:48 +0800

    init commit

---
+++ code/week2/dataloader_demo.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/25 16:19
+# @Author  : 王天赐
+# @Email   : 15565946702@163.com
+# @File    : dataloader_demo.py
+# @Software: PyCharm
+
+
+"""
+    torch.utils.data.DataLoader
+    - 功能 : 创建可以迭代的数据装载器
+"""
+
+
+class WeiBoDataset():
+
+    def __init__(self) -> None:
+        super().__init__()
